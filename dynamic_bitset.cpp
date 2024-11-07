ff2296e2a134468013d1f3dc0d4be342865446d9
vertoker <vertogpro@gmail.com>
2024-11-07 19:44:14 +0500

    fix dynamic_bitset

--- dynamic_bitset.cpp
+++ dynamic_bitset.cpp
@@ -21,16 +21,29 @@ class dynamic_bitset {
 
         bool operator*() const
         {
-	        std::uint8_t answer = (*_ptr << _offset) & BIT_LEFT;
+	        uint8_t answer = (*_ptr << _offset) & BIT_LEFT;
 	        return answer;
         }
         
-        const_iterator& operator++() { _ptr++; return *this; } // Prefix increment
-        const_iterator operator++(int) { const_iterator tmp = *this; ++(*this); return tmp; } // Postfix increment
+        const_iterator& operator++() { // Prefix increment
+            if (_offset >= 7) {
+                ++_ptr;
+                _offset = 0;
+            }
+            else {
+                ++_offset;
+            }
+            return *this;
+        }
+        const_iterator operator++(int) { // Postfix increment
+            const_iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
 
-        friend bool operator== (const const_iterator& a, const const_iterator& b)
+        [[nodiscard]] friend bool operator== (const const_iterator& a, const const_iterator& b)
             { return a._ptr == b._ptr && a._offset == b._offset; };
-        friend bool operator!= (const const_iterator& a, const const_iterator& b)
+        [[nodiscard]] friend bool operator!= (const const_iterator& a, const const_iterator& b)
             { return a._ptr != b._ptr || a._offset != b._offset; };
 
     private:
@@ -40,9 +53,12 @@ class dynamic_bitset {
 
 public:
     dynamic_bitset() = default;
-    dynamic_bitset(size_t new_size, bool value = false) 
-        { resize(new_size, value); }
     ~dynamic_bitset() = default;
+    
+    dynamic_bitset(const size_t& new_size, const bool& value = false) 
+        { resize(new_size, value); }
+    dynamic_bitset(const size_t& new_size, const size_t& new_capacity, const bool& value = false) 
+        { reserve(new_capacity); resize(new_size, value); }
     
     dynamic_bitset(dynamic_bitset&&) = default;
     dynamic_bitset& operator=(dynamic_bitset&&) = default;
@@ -52,8 +68,8 @@ public:
 
     void set(const size_t& bit_index, const bool& value = true) {
         size_t byte_index = bit_index / 8;
-	    std::uint8_t offset = static_cast<std::uint8_t>(bit_index % 8);
-	    std::uint8_t bitfield = static_cast<std::uint8_t>(BIT_LEFT >> offset);
+	    uint8_t offset = static_cast<uint8_t>(bit_index % 8);
+	    uint8_t bitfield = static_cast<uint8_t>(BIT_LEFT >> offset);
 
 	    assert(bit_index < _bit_size && "Bit index out of range");
 	    assert(byte_index < _data.size() && "Byte index out of range");
@@ -70,7 +86,7 @@ public:
 	    assert(bit_index < _bit_size && "Bit index out of range");
 	    assert(byte_index < _data.size() && "Byte index out of range");
 
-	    std::uint8_t answer = (_data[byte_index] << offset) & BIT_LEFT;
+	    uint8_t answer = (_data[byte_index] << offset) & BIT_LEFT;
 	    return answer;
     }
 
@@ -81,26 +97,26 @@ public:
 	    assert(bit_index < _bit_size && "Bit index out of range");
 	    assert(byte_index < _data.size() && "Byte index out of range");
 
-	    std::uint8_t answer = (_data[byte_index] << offset) & BIT_LEFT;
+	    uint8_t answer = (_data[byte_index] << offset) & BIT_LEFT;
 	    return answer; // take first bit
 
         // also you can remove all bits except bit field and compare as byte
         // return (data[byte_index] & bitfield) == bitfield;
     }
 
-    bool operator==(const dynamic_bitset& other) const
+    [[nodiscard]] bool operator==(const dynamic_bitset& other) const
         { return _bit_size == other._bit_size && _data == other._data; }
 
     [[nodiscard]] size_t size() const { return _bit_size; }
     [[nodiscard]] size_t capacity() const { return _data.capacity(); }
-    [[nodiscard]] std::vector<std::uint8_t> data() const { return _data; }
+    [[nodiscard]] std::vector<uint8_t> data() const { return _data; }
 
-    void reserve(size_t new_capacity) {
+    void reserve(const size_t& new_capacity) {
         size_t byte_count = GetByteCount(new_capacity);
         _data.reserve(byte_count);
     }
-
-    void resize(size_t new_size, bool value = false) {
+    
+    void resize(const size_t& new_size, const bool& value = false) {
         if (new_size > _bit_size) { // update allocated bits
             auto& last = _data.back();
             if (value) {
@@ -119,17 +135,17 @@ public:
         _bit_size = new_size;
     }
 
-    constexpr const_iterator begin() { // TODO repair
+    [[nodiscard]] const_iterator begin() { // TODO make const
         assert(_data.size() > 0 && "Can't iterate empty data");
         return const_iterator(_data.data(), 0);
     }
-    constexpr const_iterator end() {
+    [[nodiscard]] const_iterator end() {
         assert(_data.size() > 0 && "Can't iterate empty data");
 	    uint8_t offset = _bit_size % 8;
         return const_iterator(&_data.back(), offset);
     }
 
-    bool any(size_t start_range, size_t end_range, bool any_value = true) {
+    [[nodiscard]] bool any(const size_t& start_range, const size_t& end_range, const bool& any_value = true) {
 	    assert(start_range <= end_range && "end_range can't be larger than start_range");
 	    assert(end_range < _bit_size && "end_range out of range");
 
@@ -140,7 +156,7 @@ public:
         }
         return false;
     }
-    bool all(size_t start_range, size_t end_range, bool all_value = true) {
+    [[nodiscard]] bool all(const size_t& start_range, const size_t& end_range, const bool& all_value = true) {
 	    assert(start_range <= end_range && "end_range can't be larger than start_range");
 	    assert(end_range < _bit_size && "end_range out of range");
 
@@ -152,20 +168,20 @@ public:
         return true;
     }
 
-    void reset(bool value = false) {
+    void reset(const bool& value = false) {
         if (_bit_size == 0) return;
 
-        std::uint8_t byte_content = value ? ALL1 : ALL0;
+        uint8_t byte_content = value ? ALL1 : ALL0;
         for (size_t i = 0; i < _data.size(); i++)
             _data[i] = byte_content;
         // all bits, which allocated and not in size() updated too
     }
-
+    
     void clear() { _bit_size = 0; _data.clear(); }
     void shrink_to_fit() { _data.shrink_to_fit(); }
 
 private:
-    [[nodiscard]] size_t GetByteCount(size_t bit_count) {
+    [[nodiscard]] size_t GetByteCount(const size_t& bit_count) {
         size_t byte_count = 1;
         if (bit_count >= 8) {
             assert(1 + (bit_count - 1) / 8 > 0 && "Byte count must be always > 0");
@@ -175,7 +191,7 @@ private:
     }
 
     size_t _bit_size;
-    std::vector<std::uint8_t> _data;
+    std::vector<uint8_t> _data;
 
     static const uint8_t BIT_LEFT = 128; // 10000000
     static const uint8_t BIT_RIGHT = 1; // 00000001
@@ -204,10 +220,15 @@ int main() {
     std::cout << std::endl;
 
     bitset.resize(9, true);
-    bitset.resize(4);
-    bitset.resize(9);
 
     for (auto i = 0; i < 9; i++)
         std::cout << bitset[i];
+    std::cout << std::endl;
+
+    bitset.resize(4);
+    bitset.resize(7);
+    
+    for (auto it = bitset.begin(); it != bitset.end(); ++it)
+        std::cout << *it;
     std::cout << std::endl;
 }
